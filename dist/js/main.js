a82a01a1035bd8cae37ac94bef322420aee961b4
bankole <bankoleibrahim96@gmail.com>
2021-08-23 11:17:43 +0100

    first commit

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,5 @@
 const menuBtn = document.querySelector(".menu-button");
 const hamBurger = document.querySelector(".menu-button_burger");
-const nav = document.querySelector(".nav");
 const menuNav = document.querySelector(".menu-nav");
 const navItems = document.querySelectorAll(".menu-nav_items");
 
@@ -38,3 +37,24 @@ if (typed) {
     backDelay: 2000,
   });
 }
+
+const header = document.querySelector(".header");
+const main = document.querySelector(".main");
+const nav = document.querySelector(".nav");
+const navHeight = nav.getBoundingClientRect().height;
+console.log(navHeight);
+
+const stickyTop = (entries) => {
+  const [entry] = entries;
+  if (!entry.isIntersecting) {
+    nav.classList.add("sticky");
+    nav.style.position = "fixed";
+  } else nav.classList.remove("sticky");
+};
+
+const headerObserver = new IntersectionObserver(stickyTop, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+headerObserver.observe(main);
